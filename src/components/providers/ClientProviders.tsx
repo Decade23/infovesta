e4855154b314e57560b1dedd20b81f46bed149db
Dedi Fardiyanto <dedi.fardiyanto@enigmacamp.com>
2025-10-21 18:23:21 +0700

    deploy -add suspense
    Signed-off-by: Dedi Fardiyanto <dedi.fardiyanto@enigmacamp.com>

--- src/components/providers/ClientProviders.tsx
+++ src/components/providers/ClientProviders.tsx
@@ -2,11 +2,14 @@
 
 import {ToastProvider} from "@/components/ui/Toast";
 import RouteProgress from "@/lib/nprogress-client";
+import {Suspense} from "react";
 
 export default function ClientProviders({children}: { children: React.ReactNode }) {
     return (
         <>
-            <RouteProgress/>
+            <Suspense fallback={null}>
+                <RouteProgress />
+            </Suspense>
             <ToastProvider>{children}</ToastProvider>
         </>
     );
